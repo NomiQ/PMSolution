05ddcb6e1714fd60181a2669cab26ff965d1e0ee
NomiQ <nomi.qureshi85@gmail.com>
2022-10-23 19:14:36 +0100

    Updated nuget packages

--- PMSolution.Web/ViewModels/PatientViewModel.cs
+++ PMSolution.Web/ViewModels/PatientViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using PMSolution.Web.Enums;
+using PMSolution.Web.Domain;
 
 namespace PMSolution.Web.ViewModels
 {
